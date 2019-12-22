1fa1c188f56d547207c8f1f783cb3e47fd2f69a4
wyb3732466 <zhangsitun@sina.com>
2019-12-22 23:55:14 +0800

    213

--- restful-web/src/main/java/com/cf/rest/dao/impl/JedisClientCluster.java
+++ restful-web/src/main/java/com/cf/rest/dao/impl/JedisClientCluster.java
@@ -13,43 +13,36 @@ public class JedisClientCluster implements JedisClient {
 	
 	@Override
 	public String get(String key) {
-		// TODO Auto-generated method stub
 		return jedisCluster.get(key);
 	}
 
 	@Override
 	public String set(String key, String value) {
-		// TODO Auto-generated method stub
 		return jedisCluster.set(key, value);
 	}
 
 	@Override
 	public String hget(String hkey, String key) {
-		// TODO Auto-generated method stub
 		return jedisCluster.hget(hkey, key);
 	}
 
 	@Override
 	public long hset(String hkey, String key, String value) {
-		// TODO Auto-generated method stub
 		return jedisCluster.hset(hkey, key, value);
 	}
 
 	@Override
 	public long incr(String key) {
-		// TODO Auto-generated method stub
 		return jedisCluster.incr(key);
 	}
 
 	@Override
 	public long expire(String key, int second) {
-		// TODO Auto-generated method stub
 		return jedisCluster.expire(key, second);
 	}
 
 	@Override
 	public long ttl(String key) {
-		// TODO Auto-generated method stub
 		return jedisCluster.ttl(key);
 	}
 
